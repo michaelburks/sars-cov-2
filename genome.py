cfa9e333f1063b26b91832fabca2c18276a926df
Michael Burks <mburks16@gmail.com>
2020-04-09 14:34:24 -0700

    refactor segmentation

--- genome.py
+++ genome.py
@@ -1,4 +1,25 @@
-def genome():
+def string():
+  '''
+  Returns the genome sequence as a raw string.
+  '''
   with open("data/genome.txt", 'r') as input:
     genome = input.readline()
   return genome
+
+def segments():
+  '''
+  Returns the genome sequence as a list of segments (nucleotide, number of repetitions).
+  '''
+  genome_str = string()
+  genome_segments = []
+
+  seg = (genome_str[0], 1)
+  for i in range(1, len(genome_str)):
+    nuc = genome_str[i]
+    if (nuc == seg[0]):
+      seg = (nuc, seg[1]+1)
+    else:
+      genome_segments.append(seg)
+      seg = (nuc, 1)
+  genome_segments.append(seg)
+  return genome_segments
--- player.py
+++ player.py
@@ -2,22 +2,7 @@
 
 from synthesizer import Player, Synthesizer, Waveform
 
-from genome import genome
-
-genome_str = genome()
-genome_len = len(genome_str)
-
-genome_segments = []
-
-seg = (genome_str[0], 1)
-for i in range(1, genome_len):
-  nuc = genome_str[i]
-  if (nuc == seg[0]):
-    seg = (nuc, seg[1]+1)
-  else:
-    genome_segments.append(seg)
-    seg = (nuc, 1)
-genome_segments.append(seg)
+import genome
 
 player = Player()
 player.open_stream()
@@ -31,8 +16,8 @@ nuc_freqs = {'a': 440.00 * w, 'g': 392.00 * 2 * w, 'c': 523.25 * w, 't': 2793 * 
 merge_repeated_nucleotides = True
 
 if (merge_repeated_nucleotides):
-  for nuc_seg in genome_segments:
+  for nuc_seg in genome.segments():
     player.play_wave(synthesizer.generate_constant_wave(nuc_freqs[nuc_seg[0]], l * nuc_seg[1]))
 else:
-  for nucleotide in genome_str:
+  for nucleotide in genome.string():
     player.play_wave(synthesizer.generate_constant_wave(nuc_freqs[nucleotide], l))
